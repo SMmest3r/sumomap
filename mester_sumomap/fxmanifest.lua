41185595008eb587a1a6ed8d27d56d304c310a98
SM mest3r <s.mate3d@gmail.com>
2023-11-17 19:52:13 +0100

    Create fxmanifest.lua

---
+++ mester_sumomap/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "SM mest3r"
+
+description "Sumo Derby map by ᏕᎷ ᎷᏋᏕᏖ3Ꮢ. You can find my scripts here: https://mest3rdevelopment.com "
+
+this_is_a_map 'yes'
+-- Stunt models fix from https://github.com/Tobiidk/Stunt-Props-YTYP-Fix
